f2c38417818f901e879c8bbfcf55f02e23759ed4
patrictobola <tobola92@gmail.com>
2023-05-31 15:27:44 +0200

    Done dice game with bonus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('JS OK')
+// Richiamo dal DOM il bottone e il paragraph che mi serve per i risultati
+const rollDice = document.getElementById('diceButton');
+const result = document.getElementById('result');
+
+// Al click del mouse sul buttone dichiaro un vincitore
+rollDice.addEventListener('click', function(){
+
+    // Genero due variabili che calcolano un numero da 1 a 6
+    // Ne ho fatte due perché altrimenti al refresh della pagina sarebbe uscito un numero solo per entrambi i player
+    const num1 = Math.floor(Math.random() * 6 + 1);
+    const num2 = Math.floor(Math.random() * 6 + 1);
+
+    // Per questioni di leggibilità ho creato un player 1 e player 2 con i numeri creati precedentemente. Avrei potuto saltare questo step però mi sembrava più carino da leggere! :)
+    const player1 = num1;
+    const player2 = num2;
+
+    // Dichiaro la condizione di vincita e il vincitore
+    if (player1 > player2){
+        result.innerText = 'Hai vinto!!'
+    } else if (player1 < player2){
+        result.innerText = 'Il banco ha vinto!! :('
+    } else {
+        result.innerText = `Rematch! C'è stato un pareggio!!`
+    }
+})
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Stampare Uin console)
+Stampare in console
 
 - creare una variabile con giocatore uno
 - creare una variabile con giocatore due
